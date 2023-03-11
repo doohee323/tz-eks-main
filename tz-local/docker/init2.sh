bd527a98b52d31518d79726ca79a40b2481eaa3c
Doohee Hong <doohee.hong@ejn.kr>
2023-03-10 18:01:35 -0800

    init

--- tz-local/docker/init2.sh
+++ tz-local/docker/init2.sh
@@ -3,8 +3,8 @@
 PROJECT_BASE='/vagrant/terraform-aws-eks/workspace/base'
 
 cd /vagrant
-bash /vagrant/tz-local/docker/vault.sh get devops-prod ${tz_project} resources
-tar xvfz resources.zip && rm -Rf resources.zip
+#bash /vagrant/tz-local/docker/vault.sh get devops-prod ${tz_project} resources
+#tar xvfz resources.zip && rm -Rf resources.zip
 
 sudo mkdir -p /home/vagrant/.aws
 sudo cp -Rf /vagrant/resources/config /home/vagrant/.aws/config
@@ -28,29 +28,56 @@ cp -Rf /vagrant/resources/kubeconfig_${eks_project} ${PROJECT_BASE}
 
 cd ${PROJECT_BASE}
 if [ ! -d "${PROJECT_BASE}/.terraform" ]; then
-  ############################################################
   # make aws credentials
-  ############################################################
+  rm -Rf ${eks_project}*
+  ssh-keygen -t rsa -C ${eks_project} -P "" -f ${eks_project} -q
   chmod -Rf 600 ${eks_project}*
-  mkdir /home/vagrant/.ssh
+  mkdir -p /home/vagrant/.ssh
   cp -Rf ${eks_project}* /home/vagrant/.ssh
   cp -Rf ${eks_project}* /vagrant/resources
   chown -Rf vagrant:vagrant /home/vagrant/.ssh
   chown -Rf vagrant:vagrant /vagrant/resources
 
-  echo "= [terraform] =========================================="
-
   rm -Rf ${PROJECT_BASE}/lb2.tf
 
-#  terraform init
-#  terraform plan -var-file=".auto.tfvars"
-##  terraform plan | sed 's/\x1b\[[0-9;]*m//g' > a.txt
-#  terraform apply -var-file=".auto.tfvars" -auto-approve
+  terraform init
+  terraform plan -var-file=".auto.tfvars"
+#  terraform plan | sed 's/\x1b\[[0-9;]*m//g' > a.txt
+  terraform apply -var-file=".auto.tfvars" -auto-approve
+
+  export KUBECONFIG=`ls kubeconfig_${eks_project}*`
+  cp -Rf $KUBECONFIG /vagrant/config_${eks_project}
+  sudo mkdir -p /root/.kube
+  sudo cp -Rf $KUBECONFIG /root/.kube/config
+  sudo chmod -Rf 600 /root/.kube/config
+  mkdir -p /home/vagrant/.kube
+  cp -Rf $KUBECONFIG /home/vagrant/.kube/config
+  sudo chmod -Rf 600 /home/vagrant/.kube/config
+  export KUBECONFIG=/home/vagrant/.kube/config
+  sudo chown -Rf vagrant:vagrant /home/vagrant
 
   cp -Rf lb2.tf_ori lb2.tf
 
-#  terraform init
-#  terraform plan -var-file=".auto.tfvars"
-#  terraform apply -var-file=".auto.tfvars" -auto-approve
+  terraform init
+  terraform plan -var-file=".auto.tfvars"
+  terraform apply -var-file=".auto.tfvars" -auto-approve
+
+  bash /vagrant/scripts/eks_addtion.sh
+
+  echo "
+  ##[ Summary ]##########################################################
+    - in VM
+      export KUBECONFIG='/vagrant/config_${eks_project}'
+
+    - outside of VM
+      export KUBECONFIG='terraform-aws-eks/workspace/base/kubeconfig_${eks_project}'
+
+    - kubectl get nodes
+    - S3 bucket: ${s3_bucket_id}
+  #######################################################################
+  " > /vagrant/info
 fi
 
+cat /vagrant/info
+
+exit 0
--- tz-local/docker/install.sh
+++ tz-local/docker/install.sh
@@ -1,35 +1,37 @@
 #!/usr/bin/env bash
 
-#cd /Volumes/workspace/tz/tz-eks-main-t/tz-local/docker
- ##set -x
- ##shopt -s expand_aliases
- #
- #export eks_project=eks-main-t
- #export eks_domain=tzcorp.com
- #export tz_project=eks-main-t
- #export vault_token=s.H7TwKTLuJmBx0UgAg5aAeGMN
- #export docker_user="doohee.hong"
- #export docker_passwd="hdh971097"
- ##docker exec -it `docker ps | tail -n 1 | awk '{print $1}'` bash
- #
- #docker login nexus.tzcorp.com:5000 -u="${docker_user}" -p="${docker_passwd}"
- #TAG=nexus.tzcorp.com:5000/${tz_project}:latest
- ##RMI=`docker images -a | grep -w "nexus.tzcorp.com:5000/${tz_project}" | grep latest | awk '{print $3}'`
- ##docker tag ${RMI} ${TAG}
- ##docker push ${TAG}
- #echo "######### ${TAG}"
- #
- ##docker pull ${TAG}
- #cp -Rf docker-compose.yml docker-compose.yml_bak
- #sed -ie "s|tz-main|${TAG}|g" docker-compose.yml_bak
- #sed -ie "s|tz_main|${tz_project}|g" docker-compose.yml_bak
- #docker-compose -f docker-compose.yml_bak build
- ##docker-compose -f docker-compose.yml_bak build --no-cache
- #docker image ls
- #docker-compose -f docker-compose.yml_bak up -d
- ##docker-compose -f docker-compose.yml_bak down
- #docker exec -it `docker ps | grep docker-${tz_project} | awk '{print $1}'` bash
- ##docker exec -it `docker ps | grep docker_${tz_project} | awk '{print $1}'` bash
+cd /Volumes/workspace/etc/tz-eks-main/tz-local/docker
+#set -x
+#shopt -s expand_aliases
+
+export eks_project=eks-main-t
+export eks_domain=tzcorp.com
+export tz_project=devops-utils
+export vault_token=xxxxx
+export docker_user="doohee323"
+export docker_passwd="hdh971097"
+#docker exec -it `docker ps | tail -n 1 | awk '{print $1}'` bash
+
+#docker login nexus.tzcorp.com:5000 -u="${docker_user}" -p="${docker_passwd}"
+docker login -u="${docker_user}" -p="${docker_passwd}"
+#TAG=nexus.tzcorp.com:5000/${tz_project}:latest
+TAG=${docker_user}/${tz_project}:latest
+#RMI=`docker images -a | grep -w "nexus.tzcorp.com:5000/${tz_project}" | grep latest | awk '{print $3}'`
+#docker tag ${RMI} ${TAG}
+#docker push ${TAG}
+echo "######### ${TAG}"
+
+#docker pull ${TAG}
+cp -Rf docker-compose.yml docker-compose.yml_bak
+sed -ie "s|tz-main|${TAG}|g" docker-compose.yml_bak
+sed -ie "s|tz_main|${tz_project}|g" docker-compose.yml_bak
+docker-compose -f docker-compose.yml_bak build
+#docker-compose -f docker-compose.yml_bak build --no-cache
+docker image ls
+docker-compose -f docker-compose.yml_bak up -d
+#docker-compose -f docker-compose.yml_bak down
+docker exec -it `docker ps | grep docker-${tz_project} | awk '{print $1}'` bash
+#docker exec -it `docker ps | grep docker_${tz_project} | awk '{print $1}'` bash
 
 #bash /vagrant/tz-local/docker/vault.sh put devops-prod resources
 export tz_project=eks-main-t
